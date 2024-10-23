24ce84bca2348a1185396ea167a4cfe1909ab161
Igor Tudisco <igortudisco@hotmail.com>
2024-10-23 15:29:04 -0300

    Initial project

---
+++ AppLogin/Ag12/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ag12
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppLogin/Ag12/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ag12
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
